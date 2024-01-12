63348c63aa263e4d30d0a5e9a81fdc4b60eaab0a
Henry Navntoft <henrylnavntoft@gmail.com>
2024-01-12 10:38:12 +0100

    Small fixes

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function page() {
-  return <div></div>;
+export default function Home() {
+  return (
+    <>
+      <section className=" grid grid-cols-3 grid-rows-3 justify-center text-center items-center">
+        <div className="border-y-1">1</div>
+        <div className="border-y-1">2</div>
+        <div className="border-y-1">3</div>
+      </section>
+    </>
+  );
 }
